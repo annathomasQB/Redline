891c4a83654b81e77804f7f97e0657980c0f0e58
Anna Thomas <annat@qburst.com>
2020-04-23 13:56:10 +0530

    Created basic collection view

---
+++ Redline/RedlineCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RedlineCollectionViewCell.swift
+//  Redline
+//
+//  Created by AnnaThomas on 23/04/20.
+//  Copyright © 2020 QBurst Technologies. All rights reserved.
+//
+
+import UIKit
+
+class RedlineCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var contentImage: UIImageView!
+    
+}
--- Redline/ViewController.swift
+++ Redline/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IB Outlets
+    @IBOutlet weak var redLineCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+// MARK: - UICollectionView Data Source
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: RedlineCollectionViewCell.self), for: indexPath) as? RedlineCollectionViewCell {
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    
+}
